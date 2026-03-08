426358be2db06789aa7fe164909ac787ce62efc9
diamond19990628 <yaoyupeng009@gmail.com>
2026-03-08 18:37:52 +0900

    first commit

---
+++ src/main/java/com/web/flowershopping/FlowershoppingApplication.java
@@ -0,0 +1,13 @@
+package com.web.flowershopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowershoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowershoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/flowershopping/FlowershoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.flowershopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowershoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
