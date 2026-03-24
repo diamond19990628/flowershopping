d413fdafb8a8995865e241380af9f685293ea644
diamond19990628 <yaoyupeng009@gmail.com>
2026-03-25 03:16:50 +0900

    修改了一下用户实体类

--- src/main/java/com/web/flowershopping/Entity/User.java
+++ src/main/java/com/web/flowershopping/Entity/User.java
@@ -12,64 +12,53 @@ public class User {
     private String tel;
     private LocalDateTime birthday;
     private String create_datetime;
-
+    private int isAdmin;
     public int getUser_id() {
         return user_id;
     }
-
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
-
     public String getOpenid() {
         return openid;
     }
-
     public void setOpenid(String openid) {
         this.openid = openid;
     }
-
     public String getNickName() {
         return nickName;
     }
-
     public void setNickName(String nickName) {
         this.nickName = nickName;
     }
-
     public String getTel() {
         return tel;
     }
-
     public void setTel(String tel) {
         this.tel = tel;
     }
-
     public LocalDateTime getBirthday() {
         return birthday;
     }
-
     public void setBirthday(LocalDateTime birthday) {
         this.birthday = birthday;
     }
-
     public String getCreate_datetime() {
         return create_datetime;
     }
-
     public void setCreate_datetime(String create_datetime) {
         this.create_datetime = create_datetime;
     }
-
+    public int getIsAdmin() {
+        return isAdmin;
+    }
+    public void setIsAdmin(int isAdmin) {
+        this.isAdmin = isAdmin;
+    }
     @Override
     public String toString() {
-        return "User{" +
-                "user_id=" + user_id +
-                ", openid='" + openid + '\'' +
-                ", nickName='" + nickName + '\'' +
-                ", tel='" + tel + '\'' +
-                ", birthday=" + birthday +
-                ", create_datetime='" + create_datetime + '\'' +
-                '}';
+        return "User [user_id=" + user_id + ", openid=" + openid + ", nickName=" + nickName + ", tel=" + tel
+                + ", birthday=" + birthday + ", create_datetime=" + create_datetime + ", isAdmin=" + isAdmin + "]";
     }
+    
 }
