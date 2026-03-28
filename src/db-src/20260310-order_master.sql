d07529701120ee5235b84aa3202a27634c12f33f
diamond19990628 <yaoyupeng009@gmail.com>
2026-03-29 02:12:26 +0900

    修改了sql

--- src/db-src/20260310-order_master.sql
+++ src/db-src/20260310-order_master.sql
@@ -1,16 +1,34 @@
 CREATE TABLE order_master (
     order_id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '订单ID',
-    order_no VARCHAR(64) NOT NULL UNIQUE COMMENT '订单编号',
+    order_no VARCHAR(64) NOT NULL UNIQUE COMMENT '系统订单编号',
+    out_trade_no VARCHAR(64) NOT NULL UNIQUE COMMENT '微信商户订单号',
+    wx_transaction_id VARCHAR(64) DEFAULT NULL COMMENT '微信支付订单号',
+
     user_id BIGINT NOT NULL COMMENT '用户ID',
     total_amount DECIMAL(10,2) NOT NULL COMMENT '订单总金额',
+
     status_id TINYINT NOT NULL COMMENT '订单状态 0待支付 1已支付 2已完成 3已取消',
+    pay_confirm_source TINYINT DEFAULT NULL COMMENT '支付确认来源 1微信回调 2主动查单 3人工处理',
+
+    notify_status TINYINT NOT NULL DEFAULT 0 COMMENT '回调处理状态 0未收到有效回调 1已收到有效回调',
+    notify_count INT NOT NULL DEFAULT 0 COMMENT '微信回调次数',
 
     delivery_type_id TINYINT NOT NULL COMMENT '配送方式 1自取 2配送',
-    Delivery_address_id TEXT COMMENT '配送地址ID',
+    delivery_address_id BIGINT DEFAULT NULL COMMENT '配送地址ID',
+    delivery_date DATETIME DEFAULT NULL COMMENT '配送时间或者自取时间',
+
+    pay_time DATETIME DEFAULT NULL COMMENT '支付成功时间',
+    cancel_time DATETIME DEFAULT NULL COMMENT '取消时间',
+    close_reason VARCHAR(255) DEFAULT NULL COMMENT '取消/关闭原因',
+
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+
+    INDEX idx_user_id (user_id),
     INDEX idx_status_id (status_id),
-    INDEX idx_delivery_type_id (delivery_type_id)
+    INDEX idx_delivery_type_id (delivery_type_id),
+    INDEX idx_out_trade_no (out_trade_no),
+    INDEX idx_wx_transaction_id (wx_transaction_id)
 );
 CREATE TABLE order_item (
     order_id BIGINT NOT NULL,
@@ -27,14 +45,13 @@ create table card_master(
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 );
-INSERT INTO card_master(card_id,card_name) values(1,"空白卡");
-INSERT INTO card_master(card_id,card_name) values(1,"代写卡");
 create table order_status_master(
     status_id int NOT NULL PRIMARY KEY,
     status_name varchar(40) NOT NULL,
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 )
+INSERT INTO order_status_master(status_id,status_name) VALUES(0,"待支付");
 INSERT INTO order_status_master(status_id,status_name) VALUES(1,"已支付");
 INSERT INTO order_status_master(status_id,status_name) VALUES(2,"发货中");
 INSERT INTO order_status_master(status_id,status_name) VALUES(3,"已收货");
@@ -48,5 +65,3 @@ create table delivery_type_master(
 
 INSERT INTO delivery_type_master values(1,'自取');
 INSERT INTO delivery_type_master values(2,'配送');
-
-select * from order_master where status_id = 1
