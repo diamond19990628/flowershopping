33f80cbe0fce53af624e4b3d85243efd0b69217c
diamond19990628 <yaoyupeng009@gmail.com>
2026-04-01 02:49:21 +0900

    修复了数据库sql错误

--- src/db-src/20260321-information.sql
+++ src/db-src/20260321-information.sql
@@ -4,5 +4,6 @@ create table information_master(
     information_content text not null,
     publish_start_date datetime not null,
     publish_end_date datetime not null,
-    created_date datetime default current_timestamp
+    created_date datetime default current_timestamp,
+    updated_date datetime default current_timestamp on update current_timestamp
 );
