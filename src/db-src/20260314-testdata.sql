fe7089d49e62f9e8568a91ef429a3cd07731cfa9
diamond19990628 <yaoyupeng009@gmail.com>
2026-04-01 01:09:16 +0900

    修复sql错误

--- src/db-src/20260314-testdata.sql
+++ src/db-src/20260314-testdata.sql
@@ -5,7 +5,7 @@ create table delivery_address_master(
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     FOREIGN KEY (user_id) REFERENCES USER_MASTER(user_id)
-)
+);
 INSERT INTO card_master values(1,'祝福卡',now(),now());
 INSERT INTO card_master values(2,'生日卡',now(),now());
 
